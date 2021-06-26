63dd41ca19e69a13ccf5484b2efff21313dce6b9
Nikita <n.biryukov12@mail.ru>
2021-06-26 12:38:44 +0300

    Some fix

--- src/order/views.py
+++ src/order/views.py
@@ -24,6 +24,13 @@ class OrderViewSet(ModelViewSet):
         else:
             return Response(OrderSerializer(order_object).data)
 
+    def destroy(self, request, *args, **kwargs):
+        order_object = self.get_object()
+        if order_object.status != 'accepted':
+            order_object.delete()
+        return Response(OrderSerializer(order_object).data)
+
+
     @action(methods=['post'], detail=True)
     def accept(self, request, pk=None):
         order_object = self.get_object()
